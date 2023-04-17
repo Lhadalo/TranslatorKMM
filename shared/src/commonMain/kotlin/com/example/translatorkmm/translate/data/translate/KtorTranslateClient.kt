83d72cc145cf816d30b4c7cd4a48565f9b0c3379
Ola Dahl <oladahl.lel@gmail.com>
2023-04-17 20:30:19 +0200

    Setting up the translation client

---
+++ shared/src/commonMain/kotlin/com/example/translatorkmm/translate/data/translate/KtorTranslateClient.kt
@@ -0,0 +1,52 @@
+package com.example.translatorkmm.translate.data.translate
+
+import com.example.translatorkmm.NetworkConstants
+import com.example.translatorkmm.core.domain.language.Language
+import com.example.translatorkmm.translate.domain.translate.TranslateClient
+import com.example.translatorkmm.translate.domain.translate.TranslateError
+import com.example.translatorkmm.translate.domain.translate.TranslateException
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.utils.io.errors.*
+
+class KtorTranslateClient(
+    private val httpCLient: HttpClient
+): TranslateClient {
+
+    override suspend fun translate(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): String {
+        val result = try {
+            httpCLient.post {
+                url(NetworkConstants.BASE_URL + "/translate")
+                contentType(ContentType.Application.Json)
+                setBody(
+                    TranslateDto(
+                        textToTranslate = fromText,
+                        sourceLanguageCode = fromLanguage.langCode,
+                        tagetLanguageCode = toLanguage.langCode
+                    )
+                )
+            }
+        } catch (e: IOException) {
+            throw TranslateException(TranslateError.SERVICE_UNAVAILABLE)
+        }
+
+        when(result.status.value) {
+            in 200..299 -> Unit
+            500 -> throw TranslateException(TranslateError.SERVER_ERROR)
+            in 400..499 -> throw TranslateException(TranslateError.CLIENT_ERROR)
+            else -> throw TranslateException(TranslateError.UNKNOWN_ERROR)
+        }
+
+        return try {
+            result.body<TranslatedDto>().translatedText
+        } catch (e: Exception) {
+            throw TranslateException(TranslateError.SERVER_ERROR)
+        }
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/translatorkmm/translate/data/translate/TranslatedDto.kt
@@ -0,0 +1,6 @@
+package com.example.translatorkmm.translate.data.translate
+
+@kotlinx.serialization.Serializable
+data class TranslatedDto(
+    val translatedText: String
+)
