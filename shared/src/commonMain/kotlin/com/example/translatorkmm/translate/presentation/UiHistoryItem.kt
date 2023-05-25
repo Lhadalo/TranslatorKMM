fc393b600f36b081c29e22177f5ee0a6e3c0b46b
Ola Dahl <ola.dahl@smartrefill.se>
2023-05-25 15:11:44 +0200

    Shared TranslateViewModel

---
+++ shared/src/commonMain/kotlin/com/example/translatorkmm/translate/presentation/UiHistoryItem.kt
@@ -0,0 +1,11 @@
+package com.example.translatorkmm.translate.presentation
+
+import com.example.translatorkmm.core.presentation.UiLanguage
+
+data class UiHistoryItem(
+    val id: Long,
+    val fromText: String,
+    val toText: String,
+    val fromLanguage: UiLanguage,
+    val toLanguage: UiLanguage
+)
